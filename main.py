2d929e56ea2fc39cf35e8df23c34090914d80859
ftnext <takuyafjp+develop@gmail.com>
2022-08-12 23:48:21 +0900

    [feat] Auto copy HackMD template

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 
 from discord import post_discord
+from hackmd import copy_template
 
 if __name__ == "__main__":
     webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
+    hackmd_token = os.environ["HACKMD_TOKEN"]
 
-    hackmd_ops_message = """🤖HackMDの準備をお願いします。
-テンプレート https://hackmd.io/dHiRbfcKTj68SY9p9dYfnw をコピーしてTODOを解消してください"""
+    template_note_id = "1iUWLeIrS7CcluI9xA2tyw"
+    team_id = "minodriven-book-pythonista"
+    note_url = copy_template(template_note_id, team_id, hackmd_token)
+    hackmd_ops_message = f"""🤖HackMDのテンプレートをコピーしました！
+{note_url} のTODOの解消だけお願いします🙏"""
     post_discord(hackmd_ops_message, webhook_url)
 
     connpass_ops_message = """🤖connpassの準備をお願いします。
