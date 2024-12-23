6a4897d90d7b47cbf382d727bd862210926959fa
ftnext <takuyafjp+develop@gmail.com>
2024-12-24 00:11:32 +0900

    [feat] Draft user notes API client

---
+++ hackmd/src/hackmd/user_notes.py
@@ -0,0 +1,28 @@
+from hackmd.client import api_get, api_post
+
+
+def get_contents(note_id, token):
+    endpoint = f"https://api.hackmd.io/v1/notes/{note_id}"
+    return api_get(endpoint, token)
+
+
+def create_user_note(title, content, token):
+    endpoint = f"https://api.hackmd.io/v1/notes"
+    data = {
+        "title": title,
+        "content": content,
+        "readPermission": "signed_in",
+        "writePermission": "signed_in",
+    }
+    response = api_post(endpoint, data, token)
+    return f'https://hackmd.io/{response["id"]}'
+
+
+def copy_template(template_id, token):
+    template_contents = get_contents(template_id, token)
+
+    title = f'Copy of {template_contents["title"]}'
+    content = template_contents["content"]
+
+    copied_url = create_user_note(title, content, token)
+    return copied_url
