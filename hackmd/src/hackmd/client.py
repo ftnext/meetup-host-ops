f1962b9e2ad15e40a4ae6a5a3fb06e4678093cbf
ftnext <takuyafjp+develop@gmail.com>
2025-10-05 00:47:15 +0900

    [feat] Verbose option to debug

--- hackmd/src/hackmd/client.py
+++ hackmd/src/hackmd/client.py
@@ -1,5 +1,8 @@
 import json
+import logging
 from urllib.request import Request, urlopen
+
+logger = logging.getLogger(__name__)
 
 
 def api_get(url, token):
@@ -15,6 +18,11 @@ def api_post(url, data, token):
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
     }
+    logger.debug("POST %s %s", url, data)
+
     request = Request(url, data=json.dumps(data).encode(), headers=headers)
     with urlopen(request) as res:
-        return json.load(res)
+        response = json.load(res)
+
+    logger.debug("Response: %s", response)
+    return response
--- stapy_staff.py
+++ stapy_staff.py
@@ -1,8 +1,20 @@
+import argparse
+import logging
 import os
 
 from hackmd.user_notes import copy_template
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", "-v", action="store_true")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s | %(levelname)s (%(name)s) | %(filename)s:%(funcName)s:%(lineno)d - %(message)s"
+        )
+
     hackmd_token = os.environ["HACKMD_TOKEN"]
 
     template_note_id = "6LmE8fWeSnyA8aOT78ivrg"
